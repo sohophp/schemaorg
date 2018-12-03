5399627e5ae63211ede055652c1b572323093384
sohophp <sohophp@gmail.com>
2018-12-03 13:43:53 +0800

    Completion generator

--- tests/WebPageTest.php
+++ tests/WebPageTest.php
@@ -1,9 +1,24 @@
 <?php
 
-namespace Sohophp\SchemaOrg\test;
+namespace Sohophp\SchemaOrg\Test;
+
 use Sohophp\SchemaOrg\Tests\TestCase;
+use Sohophp\SchemaOrg\Thing\CreativeWork\WebPage;
 
 class WebPageTest extends TestCase
 {
-
+    public function testWebPage()
+    {
+        $WebPage = new WebPage();
+        $WebPage->name('one test')->text('one content');
+        $params = $WebPage->toArray();
+/*
+ * array(3) {
+   ["@type"]=> string(7) "WebPage"
+   ["name"]=>string(8) "one test"
+   ["text"]=>string(11) "one content"
+  }
+*/
+        $this->assertCount(3, $params);
+    }
 }
